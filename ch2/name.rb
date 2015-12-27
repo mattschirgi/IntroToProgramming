62147a7506f156b1b448f5cf59f3fef490deefd8
mattschirgi <mat.tschirgi@gmail.com>
2015-12-27 19:09:36 +0000

    ch2 exercises

---
+++ ch2/name.rb
@@ -0,0 +1,18 @@
+# modified to ask for first AND last name
+puts "Please type in your first name:"
+first_name = gets.chomp
+puts "Pretty please type in your last name:"
+last_name = gets.chomp
+puts first_name + " " + last_name
+# original program to type in first name ONLY
+puts "Please type in your name:"
+name = gets.chomp
+puts name + " is taking a programming class."
+# prints name of user 10 times
+10.times do 
+  puts name
+end
+
+
+
+  
